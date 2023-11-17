5678a6c29f412a49a0218849629bf9eef668132e
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-17 22:55:35 +0700

    complete theme

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,11 +1,61 @@
-import React from 'react';
+'use client';
+import React, {
+    Dispatch,
+    SetStateAction,
+    createContext,
+    useContext,
+    useEffect,
+    useState
+} from 'react';
 
-// const ThemeContext
+type Theme = 'light' | 'dark' | 'none';
 
-// const ThemeContextProvider = () => {
-//   return (
-//     <div>ThemeContext</div>
-//   )
-// }
+export interface ThemeContextType {
+    theme: Theme;
+    setTheme: Dispatch<SetStateAction<Theme>>;
+}
 
-// export default ThemeContext
+export const ThemeContext = createContext<ThemeContextType>({} as ThemeContextType);
+
+export const ThemeContextProvider = ({ children }: { children: React.ReactNode }) => {
+    const [theme, setTheme] = useState<Theme>('none');
+
+    useEffect(() => {
+        const themeStore = localStorage.getItem('theme');
+
+        if (themeStore) {
+            setTheme(themeStore as Theme);
+        } else {
+            setTheme('light');
+        }
+    }, []);
+
+    useEffect(() => {
+        if (theme != 'none') {
+            const body = document.getElementsByTagName('body')[0];
+            body.setAttribute('theme-data', theme as Theme);
+            localStorage.setItem('theme', theme as Theme);
+        }
+    }, [theme]);
+
+    if (theme === 'none') {
+        return <></>;
+    }
+
+    return (
+        <ThemeContext.Provider value={{ theme: theme as Theme, setTheme: setTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+
+    if (context === undefined) {
+        throw new Error('useThemeContext must be used within a ThemeContextProvider');
+    }
+
+    return context;
+};
