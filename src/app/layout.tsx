d6fca4313c3725b6c6deeb7ea3e77ba1e3558183
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-18 23:07:34 +0700

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang='en'>
-            <body className={[inter.className, 'flex min-h-screen flex-col'].join(' ')}>
-                {children}
-            </body>
+            <body className={[inter.className, 'min-h-screen'].join(' ')}>{children}</body>
         </html>
     );
 }
