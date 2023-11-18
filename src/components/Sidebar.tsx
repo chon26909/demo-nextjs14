852efe1cb8c955520f2878f9d76d87ebdf71c0b4
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-18 23:09:27 +0700

    sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <nav className='w-[250px] bg-primary'>
+            <div>sidebar</div>
+        </nav>
+    );
+};
+
+export default Sidebar;
