d43f5b70363772e3865ea0cd827fd37eec5070f6
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-21 23:33:45 +0700

    edit auth context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -3,82 +3,47 @@ import { createContext, useEffect, useState, ReactNode, useContext } from 'react
 
 import { useRouter } from 'next/navigation';
 
-export type AuthValuesType = {
-    loading: boolean;
+type AuthValuesType = {
+    login: (token: string) => void;
     logout: () => void;
-    login: () => void;
-    user: string | null;
-    setLoading: (value: boolean) => void;
-    setUser: (value: string | null) => void;
 };
 
-// ** Defaults
-const defaultProvider: AuthValuesType = {
-    user: null,
-    loading: true,
-    setUser: () => null,
-    setLoading: () => Boolean,
-    logout: () => Promise.resolve(),
-    login: () => Promise.resolve()
-};
+const AuthContext = createContext<AuthValuesType>({
+    login: () => Promise.resolve(),
+    logout: () => Promise.resolve()
+});
 
-const AuthContext = createContext(defaultProvider);
-
-type Props = {
-    children: ReactNode;
-};
-
-const AuthContextProvider = ({ children }: Props) => {
-    // ** States
-    const [user, setUser] = useState<string | null>(null);
-    const [loading, setLoading] = useState<boolean>(false);
-
-    // ** Hooks
+const AuthContextProvider = ({ children }: { children: ReactNode }) => {
     const router = useRouter();
 
     useEffect(() => {
-        const initAuth = async () => {
+        const checkToken = async () => {
             const storedToken = sessionStorage.getItem('token');
-            console.log('storedToken', storedToken);
+
             if (storedToken) {
-                // setLoading(true)
-                console.log('hello world');
-                setUser(storedToken);
-                setLoading(false);
             } else {
-                setLoading(false);
                 router.replace('/login');
             }
         };
 
-        initAuth();
+        checkToken();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const handleLogin = () => {
-        setUser('hello world');
-        //storage.setSessionToken('hello world');
+    const handleLogin = (token: string) => {
+        sessionStorage.setItem('token', token);
         router.push('/');
     };
 
     const handleLogout = () => {
-        setUser(null);
-        //storage.clearAllSession();
+        sessionStorage.removeItem('token');
         router.push('/login');
     };
 
     const values = {
-        user,
-        loading,
-        setUser,
-        setLoading,
-        logout: handleLogout,
-        login: handleLogin
+        login: handleLogin,
+        logout: handleLogout
     };
-
-    if (!user) {
-        return <div>user null</div>;
-    }
 
     return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
 };
