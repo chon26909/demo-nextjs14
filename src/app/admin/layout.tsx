6c02ceef134bf56b9ef155ac75dd733f490c8cee
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-21 10:37:08 +0700

    use auth

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,17 +3,20 @@ import NavbarAdmin from '@/components/NavbarAdmin';
 import Sidebar from '@/components/Sidebar';
 import { ThemeContextProvider } from '@/context/ThemeContext';
 import LoadingAdminLayout from './loading';
+import { AuthContextProvider } from '@/context/AuthContext';
 
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <ThemeContextProvider>
-            <NavbarAdmin />
-            <div className='flex overflow-y-hidden min-h-[calc(100vh-75px)] transition'>
-                <Sidebar />
-                <Suspense fallback={<LoadingAdminLayout />}>
-                    <main className='w-full overflow-y-scroll p-5'>{children}</main>
-                </Suspense>
-            </div>
+            <AuthContextProvider>
+                <NavbarAdmin />
+                <div className='flex overflow-y-hidden min-h-[calc(100vh-75px)] transition'>
+                    <Sidebar />
+                    <Suspense fallback={<LoadingAdminLayout />}>
+                        <main className='w-full overflow-y-scroll p-5'>{children}</main>
+                    </Suspense>
+                </div>
+            </AuthContextProvider>
         </ThemeContextProvider>
     );
 };
