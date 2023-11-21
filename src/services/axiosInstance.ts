42b7c1ee56123f6cdc7f2f07304561da8db4887b
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-21 10:36:58 +0700

    axios config

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -8,3 +8,21 @@ export const dummyApi = axios.create({
         'Content-Type': 'application/json'
     }
 });
+
+const onRequest = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+    const token = localStorage.getItem('token');
+    config.headers.Authorization = 'Bearer ' + token;
+    return config;
+};
+const onRequestError = (err: AxiosError): Promise<AxiosError> => {
+    return Promise.reject(err);
+};
+dummyApi.interceptors.request.use(onRequest, onRequestError);
+
+const onResponse = (response: AxiosResponse): AxiosResponse => {
+    return response;
+};
+const onResponseError = (err: AxiosError): Promise<AxiosError> => {
+    return Promise.reject(err.response);
+};
+dummyApi.interceptors.response.use(onResponse, onResponseError);
