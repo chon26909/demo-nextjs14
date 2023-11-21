614395107a3776daa700966bd979cccb144f7657
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-21 23:32:26 +0700

    change layout

--- src/app/(product)/layout.tsx
+++ src/app/(product)/layout.tsx
@@ -1,5 +1,5 @@
 'use client';
-import Navbar from '@/components/Navbar';
+import Navbar from './Navbar';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Suspense } from 'react';
 import Loading from './loading';
