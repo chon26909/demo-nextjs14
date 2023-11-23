9c1400b8e6d9c9854f953229e214e4c1f4eec0b8
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-23 21:41:53 +0700

    delay 3000

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -5,6 +5,8 @@ interface IRequestLogin {
     password: string;
 }
 
-export const login = (body: IRequestLogin) => {
+export const login = async (body: IRequestLogin) => {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     return dummyApi.post('/auth/login', body);
 };
