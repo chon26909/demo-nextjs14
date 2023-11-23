866fcc18d2a617447217df97b98560760832a0ca
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-23 21:25:22 +0700

    update

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -3,11 +3,51 @@
 import Button from '@/components/button/Button';
 import InputText from '@/components/input/InputText';
 import React, { useEffect, useRef } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { login } from '@/services/authService';
 
 const LoginForm = () => {
     const usernameRef = useRef<HTMLInputElement>(null);
-
     const passwordRef = useRef<HTMLInputElement>(null);
+
+    const { mutate, mutateAsync } = useMutation({ mutationFn: login });
+
+    const handleLogin = () => {
+        if (usernameRef.current?.value && passwordRef.current?.value) {
+            //
+            const data = {
+                username: usernameRef.current.value,
+                password: passwordRef.current.value
+            };
+
+            mutate(data, {
+                onSuccess: (res) => {
+                    console.log('response', res);
+                },
+                onError: (error) => {
+                    console.log('error', error);
+                }
+            });
+        }
+    };
+
+    const handleLoginAsync = async () => {
+        if (usernameRef.current?.value && passwordRef.current?.value) {
+            //
+            const data = {
+                username: usernameRef.current.value,
+                password: passwordRef.current.value
+            };
+
+            try {
+                const res = await mutateAsync(data);
+
+                console.log('res', res);
+            } catch (error) {
+                console.log('error', error);
+            }
+        }
+    };
 
     useEffect(() => {
         usernameRef.current?.focus();
@@ -20,13 +60,23 @@ const LoginForm = () => {
                     <div className='text-center text-4xl font-semibold mb-4'>Login</div>
                 </div>
                 <div>
-                    <InputText ref={usernameRef} label='Username' full />
+                    <InputText
+                        ref={usernameRef}
+                        label='Username'
+                        defaultValue={'kminchelle'}
+                        full={true}
+                    />
                 </div>
                 <div>
-                    <InputText ref={passwordRef} label='Password' full />
+                    <InputText
+                        ref={passwordRef}
+                        label='Password'
+                        defaultValue={'0lelplR'}
+                        full={true}
+                    />
                 </div>
                 <div>
-                    <Button variant='solid' full>
+                    <Button variant='solid' full={true} onClick={handleLoginAsync}>
                         Submit
                     </Button>
                 </div>
