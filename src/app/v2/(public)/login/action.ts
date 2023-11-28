a5c5b6903ca37fb4b41b6a81d2f234c1fe79ab56
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-28 23:15:46 +0700

    return message

--- src/app/v2/(public)/login/action.ts
+++ src/app/v2/(public)/login/action.ts
@@ -3,7 +3,9 @@
 import { login } from '@/services/authService';
 import { revalidatePath } from 'next/cache';
 
-export const submitLogin = async (prevState: any, formData: FormData) => {
+export const submitLogin = async (previous: any, formData: FormData) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
     console.log(formData);
 
     const username = formData.get('username') as string;
@@ -19,11 +21,11 @@ export const submitLogin = async (prevState: any, formData: FormData) => {
 
     try {
         const res = await login({ username, password });
-        console.log('res', res.data);
+        return { message: 'success', data: res.data };
     } catch (error) {
         console.log('error', error);
+        return { error: error };
     }
 
     // revalidatePath('/');
-    return { error: null };
 };
