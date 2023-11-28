412aaa965dc0ff848e3dcc9def4579fcff133bc2
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-28 23:15:32 +0700

    login form

--- src/app/v2/(public)/login/LoginForm.tsx
+++ src/app/v2/(public)/login/LoginForm.tsx
@@ -1,25 +1,37 @@
 'use client';
 import Button from '@/components/button/Button';
 import InputText from '@/components/input/InputText';
-import React, { useRef } from 'react';
-import { useFormState, useFormStatus } from 'react-dom';
+import React, { useEffect, useRef } from 'react';
+import { useFormState } from 'react-dom';
 import { submitLogin } from './action';
+import { useRouter } from 'next/navigation';
 
 type Props = {};
 
 const LoginForm = (props: Props) => {
+    const router = useRouter();
+
     const form = useRef<HTMLFormElement>(null);
 
     const username = useRef<HTMLInputElement>(null);
     const password = useRef<HTMLInputElement>(null);
 
     const [state, formAction] = useFormState(submitLogin, { error: null });
-    const { pending } = useFormStatus();
+
+    useEffect(() => {
+        const redirect = () => {
+            if (state.message == 'success') {
+                router.replace('/v2');
+            }
+        };
+
+        redirect();
+    }, [state, router]);
 
     return (
         <form
             ref={form}
-            action={(formData: FormData) => formAction(formData)}
+            action={formAction}
             className='bg-white p-7 w-[500px] shadow-lg rounded-md'
         >
             <div className='text-center text-4xl font-semibold mb-5'>Login</div>
@@ -39,11 +51,10 @@ const LoginForm = (props: Props) => {
                 defaultValue='0lelplR'
                 full={true}
             />
-            <Button type='submit' variant='solid' disabled={pending} full>
-                {pending ? 'Submiting...' : 'Submit'}
+            <Button type='submit' variant='solid' full={true}>
+                Submit
             </Button>
-            <pre>{JSON.stringify(state, null, 2)}</pre>
-            <pre>{String(pending)}</pre>
+            {/* <pre>{JSON.stringify(state, null, 2)}</pre> */}
         </form>
     );
 };
