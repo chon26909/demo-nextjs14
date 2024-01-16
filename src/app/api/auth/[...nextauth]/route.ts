3d266124001542924f77d8dc02c51c37653cf857
HiChon <59905927+chon26909@users.noreply.github.com>
2024-01-16 22:30:36 +0700

    next auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,13 @@
-import NextAuth from 'next-auth';
+import NextAuth, { AuthOptions, User } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
-const handler = NextAuth({
-    // session: {
-    //     strategy: 'jwt'
-    // },
+export const authOptions: AuthOptions = {
+    session: {
+        strategy: 'jwt'
+    },
+    jwt: {
+        secret: process.env.NEXTAUTH_SECRET
+    },
     providers: [
         CredentialsProvider({
             type: 'credentials',
@@ -12,7 +15,7 @@ const handler = NextAuth({
                 username: { label: 'username', type: 'text' },
                 password: { label: 'password', type: 'password' }
             },
-            authorize: async (credentials, req) => {
+            async authorize(credentials, req) {
                 const { username, password } = credentials as {
                     username: string;
                     password: string;
@@ -22,14 +25,43 @@ const handler = NextAuth({
                     throw new Error('invalid credentials');
                 }
 
+                const user = new Promise<User>((resolve) =>
+                    resolve({ id: '1', name: 'Hichon', email: 'chon@gmail.com', image: '' })
+                );
+
                 // confirmed users
-                return { id: 1, name: 'HiChon', email: 'chon@gmail.com' } as any;
+                return user;
             }
         })
     ],
     pages: {
         signIn: '/v2/login'
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.user = {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email,
+                    image: user.image
+                };
+            }
+
+            console.log('callback token', token);
+            console.log('callback user', user);
+
+            return token;
+        },
+        async session({ token, session }) {
+            session.user.name = token.name;
+
+            console.log('session', session);
+
+            return session;
+        }
     }
-});
+};
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
