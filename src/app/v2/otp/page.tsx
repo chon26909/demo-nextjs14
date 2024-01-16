4707323d539df8f4b4c2d03bf0db700ae0bc499d
HiChon <59905927+chon26909@users.noreply.github.com>
2024-01-16 22:32:47 +0700

    create otp page

---
+++ src/app/v2/otp/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import InputOTP from './InputOTP';
+
+type Props = {};
+
+const page = (props: Props) => {
+    return (
+        <div className='p-10'>
+            <InputOTP />
+        </div>
+    );
+};
+
+export default page;
