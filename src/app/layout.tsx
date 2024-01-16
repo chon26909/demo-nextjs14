b2e7efb324289b0e1f51b06a44eb2b8be6d3ed26
HiChon <59905927+chon26909@users.noreply.github.com>
2024-01-16 22:30:54 +0700

    edit layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.scss';
+import Providers from './providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang='en'>
             <body className={[inter.className, 'flex min-h-screen flex-col'].join()}>
-                {children}
+                <Providers>{children}</Providers>
             </body>
         </html>
     );
